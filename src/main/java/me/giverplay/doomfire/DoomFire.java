89207295012a48fd56aa9894616610e21ed56df7
GiverPlay007 <play.giver.007@gmail.com>
2020-12-08 09:24:45 -0300

    Re-import project

---
+++ src/main/java/me/giverplay/doomfire/DoomFire.java
@@ -0,0 +1,20 @@
+package me.giverplay.doomfire;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class DoomFire extends JavaPlugin
+{
+
+  @Override
+  public void onEnable()
+  {
+    // Plugin startup logic
+
+  }
+
+  @Override
+  public void onDisable()
+  {
+    // Plugin shutdown logic
+  }
+}
